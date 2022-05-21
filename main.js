2ab606e2167db7933bd8cb8304dfd8288ae530ea
DavidSKL78 <97090213+DavidSKL78@users.noreply.github.com>
2022-05-21 13:54:00 +0700

    upload uts

---
+++ main.js
@@ -0,0 +1,17 @@
+Vue.createApp({
+    data() {
+      return {
+        mesage: 'Hello Vue!',
+        header: {
+          datang:'Hai Selamat Datang',
+          nama: 'Saya David Suka Laksana',
+          uad: 'Mahasiswa Sistem Informasi UAD',
+          about: 'Nama Saya David Suka Laksana, saya lahir dan besar di Lampung tepatnya di Desa Sukabanjar, pesawaran. Saat ini saya sedang menempuh pendidikan di universitas Ahmad Dahlah Yogyakarta',
+          foto: "./img/profil.jpg",
+          foto2: "./motoran.jpeg"
+        }, 
+      }
+    }
+  }).mount('#app')
+
+  
